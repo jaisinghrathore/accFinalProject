ecd7753ac4b110d4aa6983f015277bafcf097a22
jaisinghrathore <jairqthore@gmail.com>
2022-09-21 11:55:44 +0530

    test5

--- allaps/container/src/App.js
+++ allaps/container/src/App.js
@@ -1,7 +1,12 @@
 import Authentication from "./Pages/Authentication";
 
 const App = () => {
-    return <Authentication />;
+    return (
+        <>
+            <h1>jai</h1>
+            <Authentication />
+        </>
+    );
 };
 
 export default App;
