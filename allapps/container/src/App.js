b4c3ac6a1067c6ca3eb0e3e8741d3d6af4f2f6ed
jaisinghrathore <jairqthore@gmail.com>
2022-09-22 14:52:16 +0530

    create history obj

--- allapps/container/src/App.js
+++ allapps/container/src/App.js
@@ -1,11 +1,17 @@
 import React from "react";
 import Authentication from "./Pages/Authentication";
+import { Route, Redirect, Switch } from "react-router-dom";
 
 const App = () => {
     return (
         <>
-           
-            <Authentication />
+            <Switch>
+                <Route exact path="/" component={Authentication}></Route>
+                <Route
+                    exact
+                    path="/forget_password"
+                    component={Authentication}></Route>
+            </Switch>
         </>
     );
 };
--- allapps/container/src/Pages/Authentication.js
+++ allapps/container/src/Pages/Authentication.js
@@ -1,12 +1,23 @@
 import React, { useEffect, useRef } from "react";
 import { mount } from "auth/userAuth";
+import { useHistory } from "react-router-dom";
 
 const Authentication = () => {
     const elementRef = useRef(null);
+    const history = useHistory();
 
     useEffect(() => {
-        mount(elementRef.current);
-    });
+        const { onParentNavigate } = mount(elementRef.current, {
+            onNavigate: ({ pathname: nextPath }) => {
+                const { pathname } = history.location;
+                if (pathname !== nextPath) {
+                    history.push(nextPath);
+                }
+            },
+        });
+
+        history.listen(onParentNavigate);
+    }, []);
 
     return (
         <>
