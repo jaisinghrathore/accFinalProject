3a8132919432ea5b0919c105b122a22cae000f55
jaisinghrathore <jairqthore@gmail.com>
2022-09-23 01:20:02 +0530

    added layout and routers in contaienr

--- allapps/Admin_Dashboard/src/asyncIndex.js
+++ allapps/Admin_Dashboard/src/asyncIndex.js
@@ -1,9 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import App from "./App";
 
 const mount = (element) => {
     const root = ReactDOM.createRoot(element);
-    root.render(<h1>fdsf</h1>);
+    root.render(<App />);
 };
 
 if (process.env.NODE_ENV === "development") {
