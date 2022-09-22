e669a82815f3a4554c8825411188106361610d8c
jaisinghrathore <jairqthore@gmail.com>
2022-09-23 01:17:09 +0530

    worked on admin page

--- allapps/Admin_Dashboard/src/App.js
+++ allapps/Admin_Dashboard/src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
 import Landing from "./pages/Landing";
-import Add from "./pages/Add";
+import Add from "./pages/Add    ";
 import Graph from "./pages/Graph";
 
 const App = () => {
