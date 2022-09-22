08250162306b7ee212d7ead77d782fac9f195d7a
jaisinghrathore <jairqthore@gmail.com>
2022-09-23 01:35:45 +0530

    Making admin routers work?

--- allapps/Admin_Dashboard/src/asyncIndex.js
+++ allapps/Admin_Dashboard/src/asyncIndex.js
@@ -1,16 +1,36 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { createMemoryHistory, createBrowserHistory } from "history";
 
-const mount = (element) => {
+const mount = (element, { onNavigate, defaultHistory, initialPath }) => {
+    const history =
+        defaultHistory ||
+        createMemoryHistory({
+            initialEntries: [initialPath],
+        });
+
+    if (onNavigate) {
+        history.listen(onNavigate);
+    }
+
     const root = ReactDOM.createRoot(element);
     root.render(<App />);
+
+    return {
+        onParentNavigate({ pathname: nextPath }) {
+            const { pathname } = history.location;
+            if (pathname !== nextPath) {
+                history.push(nextPath);
+            }
+        },
+    };
 };
 
 if (process.env.NODE_ENV === "development") {
     const element = document.getElementById("admin_root");
     if (element) {
-        mount(element);
+        mount(element, { defaultHistory: createBrowserHistory() });
     }
 }
 
--- allapps/container/src/Pages/admin/Admin.js
+++ allapps/container/src/Pages/admin/Admin.js
@@ -1,11 +1,22 @@
 import React, { useEffect, useRef } from "react";
-import { mount } from "admin/admin";
+import { mount } from "auth/userAuth";
+import { useHistory } from "react-router-dom";
 
-const Admin = () => {
+const Authentication = () => {
     const elementRef = useRef(null);
 
+    const history = useHistory();
+
     useEffect(() => {
-        mount(elementRef.current);
+        const { onParentNavigate } = mount(elementRef.current, {
+            initialPath: history.location.pathname,
+            onNavigate: ({ pathname: nextPathname }) => {
+                if (nextPathname !== history.location.pathname) {
+                    history.push(nextPathname);
+                }
+            },
+        });
+        history.listen(onParentNavigate);
     }, []);
 
     return (
@@ -15,4 +26,4 @@ const Admin = () => {
     );
 };
 
-export default Admin;
+export default Authentication;
