f10aabc809f2ec0597279a6b4c4068e78cc84564
jaisinghrathore <jairqthore@gmail.com>
2022-09-22 20:05:29 +0530

    container routes working

--- allapps/container/src/App.js
+++ allapps/container/src/App.js
@@ -1,13 +1,17 @@
 import React from "react";
 import Authentication from "./Pages/auth/Authentication";
 import Admin from "./Pages/admin/Admin";
-import { Route, Switch } from "react-router-dom";
+
+import { Route, Switch, Link } from "react-router-dom";
 
 const App = () => {
     return (
         <>
+            <Link to="/admin">Admin Dashboard</Link>;
+            <Link to="/auth">auth</Link>;
             <Switch>
-                <Route path="/auth" component={Authentication}></Route>
+                <Route exact path="/auth" component={Authentication}></Route>
+                <Route exact path="/admin" component={Admin}></Route>
             </Switch>
         </>
     );
